7be60466eae1f3822267034c7df99dc9dedfc105
tyagidevansh <devanshtyagi0607@gmail.com>
2024-05-26 19:45:30 +0530

    server settings modal

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 import { CreateServerModal } from "../modals/create-server-modal";
 import { InviteModal } from "../modals/invite-modal";
+import { EditServerModal } from "../modals/edit-server-modal";
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -19,6 +20,8 @@ export const ModalProvider = () => {
         <>
             <CreateServerModal/>
             <InviteModal/>
+            <EditServerModal/>
         </>
     )
 }
+
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -50,6 +50,7 @@ export const ServerHeader = ({
 
                 {isAdmin && (
                     <DropdownMenuItem 
+                        onClick={() => onOpen("editServer", { server })}
                         className="px-3 py-2 text-sm cursor-pointer">
                         Server Settings
                         <Settings className="h-4 w-4 ml-auto"/>
