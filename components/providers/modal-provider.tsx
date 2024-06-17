6d573e921b0e0a137d1028f142b61527b07ab640
tyagidevansh <devanshtyagi0607@gmail.com>
2024-06-17 20:55:10 +0530

    leave and delete servers

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -6,6 +6,8 @@ import { InviteModal } from "../modals/invite-modal";
 import { EditServerModal } from "../modals/edit-server-modal";
 import { MembersModal } from "../modals/members-modal";
 import { CreateChannelModal } from "../modals/create-channel-modal";
+import { LeaveServerModal } from "../modals/leave-server-modal";
+import { DeleteServerModal } from "../modals/delete-server-modal";
 
 export const ModalProvider = () => {
     const [isMounted, setIsMounted] = useState(false);
@@ -25,6 +27,8 @@ export const ModalProvider = () => {
             <EditServerModal/>
             <MembersModal/>
             <CreateChannelModal/>
+            <LeaveServerModal/>
+            <DeleteServerModal/>
         </>
     )
 }
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -81,6 +81,7 @@ export const ServerHeader = ({
 
                 {isAdmin && (
                     <DropdownMenuItem 
+                        onClick = {() => onOpen("deleteServer", { server })}
                         className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
                         Delete Server
                         <Trash className="h-4 w-4 ml-auto"/>
@@ -89,6 +90,7 @@ export const ServerHeader = ({
 
                 {!isAdmin && (
                     <DropdownMenuItem 
+                        onClick = {() => onOpen("leaveServer", { server })}
                         className="px-3 py-2 text-sm cursor-pointer">
                         Leave Server
                         <LogOut className="h-4 w-4 ml-auto"/>
