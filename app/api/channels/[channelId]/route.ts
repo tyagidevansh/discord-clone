3a5c0de5fc302862beba9810cc0b73d7c67bd697
tyagidevansh <devanshtyagi0607@gmail.com>
2024-06-20 00:55:35 +0530

    edit channels

--- app/api/channels/[channelId]/route.ts
+++ app/api/channels/[channelId]/route.ts
@@ -42,3 +42,50 @@ export async function DELETE (
     return new NextResponse("Internal Error", { status : 500 });
   }
 }
+
+export async function PATCH (
+  req: Request,
+  { params } : { params: {channelId: string}}
+) {
+  try {
+    const profile = await currentProfile();
+    const { Name, type } = await req.json();
+    const { searchParams } = new URL(req.url);
+    const serverId = searchParams.get("serverId")?.toString();
+
+    const server = await db.server.update({
+      where: {
+        id: serverId,
+        members: {
+          some: {
+            profileId: profile?.id,
+            role: {
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR],
+            }
+          }
+        }
+      },
+      data: {
+        channels: {
+          update: {
+            where: {
+              id: params.channelId,
+              NOT: {
+                Name: "general",
+              },
+            },
+            data: {
+              Name,
+              type
+            }
+          }
+        }
+      }
+    });
+
+    return NextResponse.json(server);
+  } catch (error) {
+    console.log("[CHANNEL_ID_PATCH]", error);
+    return new NextResponse("Internal Error", { status : 500 });
+  }
+}
--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -53,13 +53,14 @@ export const ServerChannel = ({
         <div className="ml-auto flex items-center gap-x-2">
           <ActionTooltip label = "Edit">
             <Edit
+              onClick={() => onOpen("editChannel", { server, channel })}
               className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
             />
           </ActionTooltip>
           
           <ActionTooltip label = "Delete">
             <Trash
-              onClick={() => onOpen("deleteChannel", {server, channel})}
+              onClick={() => onOpen("deleteChannel", { server, channel })}
               className="hidden group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition"
             />
           </ActionTooltip>
